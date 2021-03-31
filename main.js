23b49c6c79ffc300a56c0279fd3c0f8cf8570205
lilyhuan <61841737+lilyhuan@users.noreply.github.com>
2021-03-31 15:12:03 -0500

    modal updated to click out and close, added modal to blackberry

--- main.js
+++ main.js
@@ -98,15 +98,13 @@ function activateBag() {
 
     }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
-    }
-
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        document.getElementById('glaze-selected').removeAttribute('id')
+        document.getElementById('amount-selected').removeAttribute('id')
+        document.getElementById('bag-clickable').removeAttribute('id')
     }
     }
 }
